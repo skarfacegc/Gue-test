3ed7ad144150d2cd8f4126245d4da19a8d557444
Andrew Williams <andrew@manor.org>
2017-03-24 20:45:11 -0400

    adding a default task

--- glueyfile.js
+++ glueyfile.js
@@ -2,6 +2,8 @@ const glue = require('gluey');
 
 glue.setOption('files', 'test/**/*.test.js');
 
+glue.task('default', ['coverage','fail']);
+
 glue.task('coverage', () => {
   return glue.shell('nyc mocha {{files}}');
 });
